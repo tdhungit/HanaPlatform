f27680f02415952a7af6994e23027c0e0719b227
Jacky <tdhungit@gmail.com>
2018-03-05 22:22:38 +0700

    Default CRUD - fixed exist collection init

--- imports/common/Model.js
+++ imports/common/Model.js
@@ -1,11 +1,17 @@
 import CollectionAssign from '/imports/common/CollectionAssign';
 import Models from '/imports/collections/Models/Models';
 import SimpleSchema from 'simpl-schema';
+import {Meteor} from 'meteor/meteor';
 
 class MyModel {
     getCollection(modelName) {
         const model = Models.findOne({model: modelName});
         if (model && model.collection) {
+            const store = Meteor.connection._stores[model.collection];
+            if (store) {
+                return store._getCollection();
+            }
+
             const collection = new CollectionAssign(model.collection);
             const schemaObject = eval('(' + model.schema + ')');
             const collectionSchema = new SimpleSchema(schemaObject);
