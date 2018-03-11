dcbbd356fa9e31457012f5c1939e6e94a941f519
Jacky <tdhungit@gmail.com>
2018-03-11 18:41:38 +0700

    Core Component - add filter

--- imports/ui/default/pages/models/components/ListComponent.js
+++ imports/ui/default/pages/models/components/ListComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router';
 import {
+    Row,
+    Col,
     Table,
     Alert
 } from 'reactstrap';
@@ -13,9 +15,19 @@ import BootstrapPaginator from 'react-bootstrap-pagination';
 import {T} from '/imports/common/Translation';
 import container from '/imports/common/Container';
 import Loading from '../../../components/Loading/Loading';
-import {FieldView} from '../../../components/Fields/Fields';
+import {FieldView, FieldButton, FieldInput} from '../../../components/Fields/Fields';
+import {utilsHelper} from '../../../helpers/utils/utils';
+import {t} from '/imports/common/Translation';
 
 class ListComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filters: {}
+        };
+    }
+
     renderHeader() {
         const model = this.props.model;
 
@@ -28,6 +40,44 @@ class ListComponent extends Component {
         headers.push(<th key="actions"></th>);
 
         return headers;
+    }
+
+    renderFilterForm() {
+        let filters = [];
+        for (let fieldName in this.props.model.list.fields) {
+            let field = this.props.model.list.fields[fieldName];
+            filters.push(
+                <td key={fieldName}>
+                    <FieldInput name={fieldName}
+                                type={field.type}
+                                options={field.options}
+                                placeholder={t.__(field.label)}
+                                value={this.getFilter(fieldName)}
+                                onChange={this.onFilter.bind(this)}/>
+                </td>
+            )
+        }
+
+        filters.push(
+            <td key="actions">
+                <FieldButton label="Filter" color="primary" onClick={this.handleFilter.bind(this)}/>
+            </td>
+        );
+
+        return filters;
+    }
+
+    onFilter(event) {
+        const filters = utilsHelper.inputChange(event, this.state.filters);
+        this.setState({filters: filters});
+    }
+
+    getFilter(filter) {
+        return utilsHelper.getField(this.state.filters, filter);
+    }
+
+    handleFilter() {
+        this.props.pagination.filters(this.state.filters);
     }
 
     renderCol(record) {
@@ -74,21 +124,29 @@ class ListComponent extends Component {
     }
 
     render() {
-        return this.props.records.length > 0 ? (
-            <div>
-                <Table responsive hover>
-                    <thead>
-                    <tr>{this.renderHeader()}</tr>
-                    </thead>
-                    <tbody>
-                    {this.renderRows()}
-                    </tbody>
-                </Table>
-                <BootstrapPaginator pagination={this.props.pagination}
-                                    limit={this.props.limit}
-                                    containerClass="text-right"/>
-            </div>
-        ) : <Alert color="warning"><T>Empty Data.</T></Alert>
+        return (
+            <Row>
+                <Col>
+                    <Table responsive hover>
+                        <thead>
+                        <tr>{this.renderHeader()}</tr>
+                        </thead>
+                        <tbody>
+                        {this.renderFilterForm()}
+                        {this.props.records.length > 0
+                            ? this.renderRows()
+                            : null}
+                        </tbody>
+                    </Table>
+                    {this.props.records.length <= 0
+                        ? <Alert color="warning"><T>Empty Data.</T></Alert>
+                        : null}
+                    <BootstrapPaginator pagination={this.props.pagination}
+                                        limit={this.props.limit}
+                                        containerClass="text-right"/>
+                </Col>
+            </Row>
+        );
     }
 }
 
